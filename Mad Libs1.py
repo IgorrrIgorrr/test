66d13e88953b4bc5b0e6742657c17bacdf22201e
IgorrrIgorrr <113926803+IgorrrIgorrr@users.noreply.github.com>
2023-03-06 14:18:56 +0400

    ф

---
+++ Mad Libs1.py
@@ -0,0 +1,28 @@
+import random
+
+print("Hello, my friend.")
+a = random.randrange(1, 4)
+print(a)
+if a == 1:
+    number = input("input a number:")
+    Measure_of_time = input("input Measure of time:")
+    Mode_of_Transportation = input("input Mode of Transportation:")
+    Adjective = input("input Adjective:")
+    Adjective2 = input("input Adjective2:")
+    Noun = input("input noun:")
+    Color = input("input Color:")
+    Part_of_the_Body = input("input Part of the Body:")
+    verb = input("Verb:")
+    Number2 = input("Number2:")
+    Noun2 = input("Noun2:")
+    Noun3 = input("Noun3:")
+    Part_of_the_Body_2 = input("Part of the Body 2:")
+    Noun4 = input("Noun4:")
+    Adjective3 = input("Adjective3:")
+    Silly_Word = input("Silly Word:")
+
+    print("It was about", Number, Measure_of_time, "ago when I arrived at the hospital in a ", Mode_of_Transportation, ". The hospital is a/an ", Adjective, " place, there are a lot of ", Adjective2, Noun, "here. There are nurses here who have ", Color, Part_of_the_Body, ". If someone wants to come into my room I told them that they have to", Verb, " first. I’ve decorated my room with ", Number2, Noun2, ". Today I talked to a doctor and they were wearing a ", Noun3,  ") on their", Part_of_the_Body_2, ". I heard that all doctors (Verb) (Noun4) every day for breakfast. The most ( Adjective3) thing about being in the hospital is the (Silly Word ) (Noun) !")
+elif a == 2:
+    print("два")
+else:
+    print("jjjj")
